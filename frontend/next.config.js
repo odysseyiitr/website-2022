4b745b91738f616380371b69d13ac9a568cda3bb
Aditya Bisht <adityabisht64@gmail.com>
2022-10-28 04:17:25 +0530

    Minor fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,13 +12,6 @@ const nextConfig = {
 
     return config
   },
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
 };
 
 module.exports = nextConfig
