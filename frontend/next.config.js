726a2114300d0e7f2092fe511301480d140bd92c
Aditya Bisht <adityabisht64@gmail.com>
2022-10-28 01:55:16 +0530

    Add Hot Reload in Docker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,13 @@ const nextConfig = {
 
     return config
   },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 module.exports = nextConfig
