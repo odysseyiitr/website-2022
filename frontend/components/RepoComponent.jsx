642962b1b8eb6788ee51c7a25c7685b9715ab080
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2022-12-05 02:17:25 +0530

    test pick issues and other css changes

--- frontend/components/RepoComponent.jsx
+++ frontend/components/RepoComponent.jsx
@@ -7,6 +7,10 @@ const axios = require("axios").default;
 const Repo = ({ Card, callback }) => {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(true);
+  if (Card.claim == true)
+    var cardColour = "none";
+  else
+    var cardColour = "linear-gradient(90deg, rgba(50, 18, 138, 0.207) 0%, rgba(80, 41, 189, 0.207) 0.01%, rgba(170, 28, 100, 0.237) 58.85%, rgba(233, 69, 96, 0.3) 100%), linear-gradient(90deg, #1D0F44 0%, #3D0E3D 100%)";
 
   async function claimIssue(Card) {
     if (loading) {
@@ -24,7 +28,7 @@ const Repo = ({ Card, callback }) => {
 
 
   return (
-    <div className="repobox">
+    <div className="repobox" style={{ background: cardColour }}>
       <div className="cardTag">{Card.tag}</div>
       <div className="issueDetails">
         <div>
@@ -56,7 +60,7 @@ const Repo = ({ Card, callback }) => {
         )}
 
         <div >
-          {Card.claim  == false ? (
+          {Card.claim == false ? (
             <button
               className="button"
               style={{
