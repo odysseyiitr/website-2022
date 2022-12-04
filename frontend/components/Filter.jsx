b1c3791f20115fb4e0589e24cc0de58fdd516bf4
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2022-12-04 06:04:35 +0530

    adds dropdown to filter

--- frontend/components/Filter.jsx
+++ frontend/components/Filter.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import NavItem from "./NavItem"
 
 const Filter = () => {
     return (
         <>
-            <div className="filter">
-                <div>
-                    <p>Filter</p>
+            <div className="filterDropDown">
+                <div className="filter">
+                    <div>
+                        <p>Filter</p>
+                    </div>
+                    <div className="filterIcon">
+                        <img src="/images/filter_icon.svg" />
+                    </div>
+
                 </div>
-                <div className="filterIcon">
-                    <img src="/images/filter_icon.svg"/>
+                <div className="filterMenu">
+                    <button className="easy"><div>easy</div></button>
+                    <button className="medium"><div>medium</div></button>
+                    <button className="hard"><div>hard</div></button>
+                    <button className="claimed"><div>claimed</div></button>
+                    <button className="unclaimed"><div>unclaimed</div></button>
                 </div>
             </div>
         </>
