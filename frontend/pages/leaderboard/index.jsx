94003d3f936181ac97a03e6c41fbef4643acf297
nik-55 <nik.xyz.in@gmail.com>
2022-12-05 00:06:53 +0530

    chore: added media query in leaderboard

--- frontend/pages/leaderboard/index.jsx
+++ frontend/pages/leaderboard/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import LeaderBoardTable from "./LeaderBoardTable";
-import RepoTable from "./RepoTable";
 
 const LeaderBoard = () => {
   return (
     <>
       <LeaderBoardTable />
-      {/* <RepoTable /> */}
     </>
   );
 };
