94cc4925cd5bfd18bd324244d9f592b59c584d3c
Pratham <prathamks.email@gmail.com>
2022-12-07 21:56:07 +0530

    make pagination bar

--- frontend/components/Announcements.jsx
+++ frontend/components/Announcements.jsx
@@ -1,24 +1,54 @@
 import AnouncementCard from "./AnouncementCard";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import React, { useState } from "react";
+const currentPage = 0;
 
 const Announcement = ({ data }) => {
+  var data = [{date:"abc", heading:"abc", description:"abc", time:"abc", venue:"abc", note:"abc", index_no:2},{date:"abc", heading:"abc", description:"abc", time:"abc", venue:"abc", note:"abc", index_no:1},{date:"abc", heading:"abc", description:"abc", time:"abc", venue:"abc", note:"abc", index_no:1},{date:"abc", heading:"abc", description:"abc", time:"abc", venue:"abc", note:"abc", index_no:1}] 
+  const [ABC,setABC]=useState(false);
+  var pages = Math.ceil(data.length/3);
+  // var pages =5;
+  var pagearray=[];
+  for (let i=0; i< pages; i++){
+    pagearray.push(i);
+  }
+  function page(x){
+    if (x>currentPage){
+      console.log(currentPage)
+      var shift=x-currentPage;
+      currentPage=x;
+      setABC(current => !current)
+      console.log(ABC)
+    }
+    else {
+      console.log(currentPage)
+        var shift=currentPage-x;
+        currentPage=x;
+        setABC(current => !current)
+        console.log(ABC)
+    }
+  }
   function slideLeft() {
     var slider = document.getElementById("anouncementList");
     if (!slider) {
       return;
     }
+    currentPage=(currentPage-1)%pages;
+    setABC(current => !current);
     slider.scrollLeft =
       slider.scrollLeft -
-      (document.getElementsByClassName("card")[0].scrollWidth + 35) * 3;
+      (document.getElementsByClassName("anouncementCard")[0].width + 35) * 3;
   }
   function slideRight() {
     var slider = document.getElementById("anouncementList");
     if (!slider) {
       return;
     }
+    currentPage=(currentPage+1)%pages;
+    setABC(current => !current);
     slider.scrollLeft =
       slider.scrollLeft +
-      (document.getElementsByClassName("card")[0].scrollWidth + 35) * 3;
+      (document.getElementsByClassName("anouncementCard")[0].width + 35) * 3;
   }
   return (
     <div className="announcement">
@@ -55,6 +85,13 @@ const Announcement = ({ data }) => {
           }}
         />
       </div>
+      <div className="pagination" id="pagination">
+          {pagearray.map((item, i) => {
+            return (
+              (currentPage === i) ? (<div className="selected" id="selected" onClick={() => page(i)}></div>) : (<div className="unselected" id="unselected" onClick={() => page(i)}></div>)
+            );
+            })}
+      </div>        
     </div>
   );
 };
