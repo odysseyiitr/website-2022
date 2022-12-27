e8165e2527873de53e5786bb653fd74f4d559cd2
Anand Kumar <anand_k@cs.iitr.ac.in>
2022-12-27 12:27:08 +0530

    fix: add keys to components in frontend

--- frontend/components/Announcements.jsx
+++ frontend/components/Announcements.jsx
@@ -1,17 +1,16 @@
 import AnouncementCard from "./AnouncementCard";
 import React, { useState } from "react";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 const Announcement = ({ data }) => {
-  var array=[];
-  for(let i=0; i<data.length; i++) {
-    if (i%3==0 && i!=0){
+  var array = [];
+  for (let i = 0; i < data.length; i++) {
+    if (i % 3 == 0 && i != 0) {
       array.push(subarr);
-      var subarr=[];
+      var subarr = [];
       subarr.push(i);
-    }
-    else{
-      subarr.push(i)
+    } else {
+      subarr.push(i);
     }
   }
   return (
@@ -19,26 +18,29 @@ const Announcement = ({ data }) => {
       <h1>Announcement</h1>
 
       <div className="anouncebox">
-      <Carousel showArrows={true} showThumbs={false}>
-        {array.map((page, i)=>{
-          return (
-            <div className="carouselBox">
-            {page.map((item,j) => {
-              return (<AnouncementCard
-                date={data[item].date}
-                index_no={item}
-                heading={data[item].title}
-                description={data[item].description}
-                venue={data[item].venue}
-                time={data[item].time}
-                note={data[item].note}
-              />)
-            })}
-            </div>
-          )
-        })}
-      </Carousel>
-    </div>
+        <Carousel showArrows={true} showThumbs={false}>
+          {array.map((page, i) => {
+            return (
+              <div key={i} className="carouselBox">
+                {page.map((item, j) => {
+                  return (
+                    <AnouncementCard
+                      key={j}
+                      date={data[item].date}
+                      index_no={item}
+                      heading={data[item].title}
+                      description={data[item].description}
+                      venue={data[item].venue}
+                      time={data[item].time}
+                      note={data[item].note}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
     </div>
   );
 };
